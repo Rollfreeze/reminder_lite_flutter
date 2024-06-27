35fdd2d537b4f94ec17bcc430aafb4ccc71d04d2
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-06-27 20:55:34 +0300

    fix: AppLocalizations.of(context) is null

--- reminder_lite/lib/main.dart
+++ reminder_lite/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:reminder_lite/l10n/l10n.dart';
 import 'package:reminder_lite/services/router.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,6 +18,7 @@ class MainApp extends StatelessWidget {
       routerConfig: AppRouter.router,
       supportedLocales: L10n.all,
       localizationsDelegates: const [
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
