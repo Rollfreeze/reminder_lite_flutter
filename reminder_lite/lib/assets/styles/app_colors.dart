89e8be8a51433ffa15d8a5445ff4552e02305d41
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-06-28 23:20:01 +0300

    chore: AppColors

---
+++ reminder_lite/lib/assets/styles/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+abstract class AppColors {
+  /// Primary.
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
+  static const blue = Color(0xFF007AFF);
+  static const red = Color(0xFFFF3B30);
+
+  /// Secondary.
+  static const gray1 = Color(0xFFC7C7CC);
+  static const gray2 = Color(0xFFABB0BA);
+  static const gray3 = Color(0xFF3C3C43);
+  static const gray4 = Color(0xFFEBF0F7);
+
+  /// Text color.
+  static const text1 = Color(0xFF000000);
+  static const text2 = Color(0xFF3A3A3C);
+  static const text3 = Color(0xFF8E8E93);
+}
