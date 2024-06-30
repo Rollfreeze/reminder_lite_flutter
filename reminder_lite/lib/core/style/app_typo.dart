82641f5de346199e755244a1de036080a972fcfb
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-06-30 20:16:12 +0300

    chore: added AppTypo

---
+++ reminder_lite/lib/core/style/app_typo.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:reminder_lite/core/style/app_colors.dart';
+
+class AppTypo {
+  static const semibold32 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w600,
+    fontSize: 32,
+    height: 22 / 32,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const semibold18 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 22 / 18,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const semibold16 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 22 / 16,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const regular17 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w400,
+    fontSize: 17,
+    height: 22 / 17,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const bold17 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w700,
+    fontSize: 17,
+    height: 22 / 17,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const medium17 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w500,
+    fontSize: 17,
+    height: 22 / 17,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const regular16 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 22 / 16,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const regular15 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w400,
+    fontSize: 15,
+    height: 22 / 15,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const semibold13 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+    height: 22 / 13,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static const regular10 = TextStyle(
+    color: AppColors.textDefault,
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+    height: 22 / 10,
+    letterSpacing: -0.4,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+}
