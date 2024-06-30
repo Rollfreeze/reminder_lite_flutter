a7812758e7ef63a8ab3ee6e17b4521d833409758
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-06-30 22:46:18 +0300

    chore: added AppShadows

---
+++ reminder_lite/lib/core/style/app_shadows.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+/// App shadows keep class.
+class AppShadows {
+  static const defaultShadow = [
+    BoxShadow(
+      offset: Offset(0, 2),
+      blurRadius: 24,
+      spreadRadius: 0,
+      color: Color(0x00C9C9C9),
+    ),
+  ];
+}
