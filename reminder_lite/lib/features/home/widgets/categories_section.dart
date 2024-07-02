138e935e9b3771b15aa3d1d785d9fd6852dd1339
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-07-02 21:44:28 +0300

    wip: CupertinoSearchTextField widget

---
+++ reminder_lite/lib/features/home/widgets/categories_section.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'package:reminder_lite/features/home/pages/home_page.dart';
+import 'package:reminder_lite/features/home/widgets/category_button.dart';
+
+/// A section with all reminder categories on [HomePage].
+class CategoriesSection extends StatelessWidget {
+  const CategoriesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          children: [
+            Flexible(
+              child: CategoryButton.today(
+                count: 10,
+                onTap: () => context.go('/today'),
+              ),
+            ),
+            const SizedBox(width: 13),
+            Flexible(
+              child: CategoryButton.forMonth(
+                count: 20,
+                onTap: () => context.go('/month'),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 13),
+        Row(
+          children: [
+            Flexible(
+              child: CategoryButton.all(
+                count: 30,
+                onTap: () => context.go('/all'),
+              ),
+            ),
+            const SizedBox(width: 13),
+            Flexible(
+              child: CategoryButton.done(
+                count: 40,
+                onTap: () => context.go('/done'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
