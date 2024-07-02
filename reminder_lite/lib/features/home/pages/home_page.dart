87aadf21fff52f18a71b5acc7e389ab9b60d1dec
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-07-02 21:44:01 +0300

    wip: CategoriesSection

--- reminder_lite/lib/features/home/pages/home_page.dart
+++ reminder_lite/lib/features/home/pages/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:go_router/go_router.dart';
 import 'package:reminder_lite/core/style/app_colors.dart';
-import 'package:reminder_lite/features/home/widgets/category_button.dart';
+import 'package:reminder_lite/features/home/widgets/categories_section.dart';
 
 /// This is a home page with all reminder groups.
 class HomePage extends StatelessWidget {
@@ -16,45 +15,11 @@ class HomePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: ListView(
-            children: [
-              const SizedBox(height: 10),
-              const CupertinoSearchTextField(),
-              const SizedBox(height: 26),
-              Row(
-                children: [
-                  Flexible(
-                    child: CategoryButton.today(
-                      count: 10,
-                      onTap: () => context.go('/today'),
-                    ),
-                  ),
-                  const SizedBox(width: 13),
-                  Flexible(
-                    child: CategoryButton.forMonth(
-                      count: 20,
-                      onTap: () => context.go('/month'),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 13),
-              Row(
-                children: [
-                  Flexible(
-                    child: CategoryButton.all(
-                      count: 30,
-                      onTap: () => context.go('/all'),
-                    ),
-                  ),
-                  const SizedBox(width: 13),
-                  Flexible(
-                    child: CategoryButton.done(
-                      count: 40,
-                      onTap: () => context.go('/done'),
-                    ),
-                  ),
-                ],
-              ),
+            children: const [
+              SizedBox(height: 10),
+              CupertinoSearchTextField(),
+              SizedBox(height: 26),
+              CategoriesSection(),
             ],
           ),
         ),
