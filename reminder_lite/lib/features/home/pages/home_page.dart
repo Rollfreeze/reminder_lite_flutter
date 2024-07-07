e878a836601a133b625e3849c99817c348e759cf
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-07-07 17:54:06 +0300

    feat: progress_slider

--- reminder_lite/lib/features/home/pages/home_page.dart
+++ reminder_lite/lib/features/home/pages/home_page.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reminder_lite/core/style/app_colors.dart';
-import 'package:reminder_lite/features/home/domain/bloc/progress_bloc/progress_bloc.dart';
-import 'package:reminder_lite/features/home/domain/models/reminder_categories.dart';
 import 'package:reminder_lite/features/home/widgets/categories_section/categories_section.dart';
 import 'package:reminder_lite/features/home/widgets/progress_section/progress_section.dart';
 
 /// This is a home page with all reminder groups.
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final _controller = PageController();
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,31 +19,13 @@ class _HomePageState extends State<HomePage> {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: ListView(
-            children: [
-              const SizedBox(height: 10),
-              const CupertinoSearchTextField(),
-              const SizedBox(height: 25),
-              const CategoriesSection(),
-              const SizedBox(height: 25),
-              BlocBuilder<ProgressBloc, ProgressState>(
-                builder: (_, state) {
-                  final bloc = context.read<ProgressBloc>();
-                  return ProgressSection(
-                    onTodayPressed: () => bloc.add(ProgressEvent.selectedCategory(TodayCategory())),
-                    onForMonthPressed: () => bloc.add(ProgressEvent.selectedCategory(ForMonthCategory())),
-                    onAllPressed: () => bloc.add(ProgressEvent.selectedCategory(AllCategory())),
-                    selectedCategory: state.category,
-                    items: state.items,
-                    controller: _controller,
-                    onPageChanged: (index) => switch (index) {
-                      0 => bloc.add(ProgressEvent.selectedCategory(TodayCategory())),
-                      1 => bloc.add(ProgressEvent.selectedCategory(ForMonthCategory())),
-                      2 => bloc.add(ProgressEvent.selectedCategory(AllCategory())),
-                      _ => null,
-                    },
-                  );
-                },
-              ),
+            children: const [
+              SizedBox(height: 10),
+              CupertinoSearchTextField(),
+              SizedBox(height: 25),
+              CategoriesSection(),
+              SizedBox(height: 25),
+              ProgressSection(),
             ],
           ),
         ),
--- reminder_lite/lib/features/home/widgets/progress_section/progress_section.dart
+++ reminder_lite/lib/features/home/widgets/progress_section/progress_section.dart
@@ -1,63 +1,89 @@
 import 'package:flutter/cupertino.dart';
-import 'package:reminder_lite/features/home/domain/models/progress_circle_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reminder_lite/features/home/domain/bloc/progress_bloc/progress_bloc.dart';
 import 'package:reminder_lite/features/home/domain/models/reminder_categories.dart';
 import 'package:reminder_lite/features/home/pages/home_page.dart';
 import 'package:reminder_lite/features/home/widgets/progress_section/categories_row.dart';
 import 'package:reminder_lite/features/home/widgets/progress_section/progress_circle_section.dart';
 
 /// A section with reminder categories progress on [HomePage].
-class ProgressSection extends StatelessWidget {
-  /// On [TodayCategory] pressed.
-  final VoidCallback onTodayPressed;
+class ProgressSection extends StatefulWidget {
+  const ProgressSection({super.key});
 
-  /// On [ForMonthCategory] pressed.
-  final VoidCallback onForMonthPressed;
+  @override
+  State<ProgressSection> createState() => _ProgressSectionState();
+}
 
-  /// on [AllCategory] pressed.
-  final VoidCallback onAllPressed;
+class _ProgressSectionState extends State<ProgressSection> {
+  final _controller = PageController();
+  late final ProgressBloc _bloc;
+  int? _desirePage;
 
-  /// A chosen reminder category to show.
-  final ReminderCategory selectedCategory;
+  @override
+  void initState() {
+    super.initState();
+    _bloc = context.read<ProgressBloc>();
+  }
 
-  /// Progress circles' items.
-  final List<ProgressCircleModel> items;
-
-  /// A circles' slider controller.
-  final PageController controller;
-
-  /// When changes a current circle page.
-  final ValueChanged<int> onPageChanged;
-
-  const ProgressSection({
-    super.key,
-    required this.onTodayPressed,
-    required this.onForMonthPressed,
-    required this.onAllPressed,
-    required this.selectedCategory,
-    required this.items,
-    required this.controller,
-    required this.onPageChanged,
-  });
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        CategoriesRow(
-          onTodayPressed: onTodayPressed,
-          onForMonthPressed: onForMonthPressed,
-          onAllPressed: onAllPressed,
-          selectedCategory: selectedCategory,
-        ),
-        const SizedBox(height: 18),
-        ProgressCircleSection(
-          items: items,
-          controller: controller,
-          onPageChanged: onPageChanged,
-        ),
-      ],
+    return BlocConsumer<ProgressBloc, ProgressState>(
+      listener: (_, state) {},
+      builder: (_, state) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            CategoriesRow(
+              onTodayPressed: () => _animateToPage(0),
+              onForMonthPressed: () => _animateToPage(1),
+              onAllPressed: () => _animateToPage(2),
+              selectedCategory: state.category,
+            ),
+            const SizedBox(height: 18),
+            ProgressCircleSection(
+              items: state.items,
+              controller: _controller,
+              onPageChanged: _onPageChanged,
+            ),
+          ],
+        );
+      },
     );
+  }
+
+  void _animateToPage(int index) {
+    if (index == _controller.page) return;
+
+    _desirePage = index;
+
+    _controller.animateToPage(
+      index,
+      duration: const Duration(milliseconds: 750),
+      curve: Curves.ease,
+    );
+  }
+
+  void _onPageChanged(int index) {
+    if ((_desirePage != null) && (index != _desirePage)) {
+      return;
+    }
+
+    if (_desirePage != null) {
+      _desirePage = null;
+    }
+
+    return switch (index) {
+      0 => _bloc.add(ProgressEvent.selectedCategory(TodayCategory())),
+      1 => _bloc.add(ProgressEvent.selectedCategory(ForMonthCategory())),
+      2 => _bloc.add(ProgressEvent.selectedCategory(AllCategory())),
+      _ => null,
+    };
   }
 }
