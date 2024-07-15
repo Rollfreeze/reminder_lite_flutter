3afe26415c5f10c4abdc0e6d8024030b6f402f29
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-07-15 23:08:09 +0300

    chore: deps_show update

--- reminder_lite/tools/deps_show.sh
+++ reminder_lite/tools/deps_show.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd ..
-lakos lib/ -i "{**.freezed.dart,**.g.dart,**.config.dart,**.gr.dart,routes.dart,di.dart}" --metrics > deps.dot
+lakos lib/ -i "{**.freezed.dart,**.g.dart,**.config.dart,**.gr.dart,l10n/**,core/**}" --metrics > deps.dot
 dot -Tpng -Gdpi=200 -o deps.png deps.dot
