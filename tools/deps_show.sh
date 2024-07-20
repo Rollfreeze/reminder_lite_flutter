2e29c43dcc988cf720c6b614cd81c18970f786b5
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-07-20 10:22:31 +0300

    chore: deps show sh update

--- tools/deps_show.sh
+++ tools/deps_show.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+exclude1='**.freezed.dart'
+exclude2='**.g.dart'
+exclude3='**.gr.dart'
+exclude4='l10n/**'
+exclude5='core/**'
+
 cd ..
-lakos lib/ -i "{**.freezed.dart,**.g.dart,**.config.dart,**.gr.dart,l10n/**,core/**}" --metrics > deps.dot
+lakos lib/ -i "{$exclude1,$exclude2,$exclude3,$exclude4,$exclude5}" --metrics > deps.dot
 dot -Tpng -Gdpi=200 -o deps.png deps.dot
