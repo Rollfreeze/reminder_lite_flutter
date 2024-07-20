f188f4f45e9c877555fbf45f3661731ec25b5690
Egor <71181173+Rollfreeze@users.noreply.github.com>
2024-07-20 09:45:10 +0300

    docs: HomePageWrapper

--- lib/features/home/pages/home_page_wrapper.dart
+++ lib/features/home/pages/home_page_wrapper.dart
@@ -3,8 +3,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:reminder_lite/core/services/localization_service.dart';
 import 'package:reminder_lite/features/home/domain/bloc/progress_bloc/progress_bloc.dart';
+import 'package:reminder_lite/features/home/pages/home_page.dart';
 
-/// Di wrapper.
+/// Di wrapper for [HomePage].
 @RoutePage()
 class HomePageWrapper extends AutoRouter implements AutoRouteWrapper {
   const HomePageWrapper({super.key});
