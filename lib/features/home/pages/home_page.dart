2f444cded7ba5a4eada9bfdd73361b386577f15c
Rollfire <goronos2000@gmail.com>
2024-10-25 23:52:17 +0300

    feat: app bar inside bottom sheet

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,7 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:reminder_lite/core/services/localization_service.dart';
 import 'package:reminder_lite/core/style/app_colors.dart';
+import 'package:reminder_lite/core/style/app_radius.dart';
+import 'package:reminder_lite/core/style/app_typo.dart';
 import 'package:reminder_lite/core/widgets/add_reminder_button.dart';
 import 'package:reminder_lite/features/home/widgets/categories_section/categories_section.dart';
 import 'package:reminder_lite/features/home/widgets/progress_section/progress_section.dart';
@@ -39,17 +43,43 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               AddReminderButton(
-                onPressed: () {
-                  showCupertinoModalBottomSheet(
-                    context: context,
-                    builder: (context) {
-                      return const ColoredBox(
-                        color: AppColors.blue,
-                        child: Column(),
-                      );
-                    },
-                  );
-                },
+                onPressed: () => showCupertinoModalBottomSheet(
+                  context: context,
+                  builder: (context) => CupertinoPageScaffold(
+                    backgroundColor: AppColors.gray6,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 86,
+                              child: CupertinoButton(
+                                onPressed: () {},
+                                child: Text(LocalizationService.locale.cancel),
+                              ),
+                            ),
+                            const Spacer(),
+                            Text(
+                              LocalizationService.locale.newReminder,
+                              style: const TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                            const Spacer(),
+                            SizedBox(
+                              width: 86,
+                              child: CupertinoButton(
+                                onPressed: () {},
+                                child: Text(
+                                  LocalizationService.locale.add,
+                                  style: const TextStyle(fontWeight: FontWeight.w600),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
