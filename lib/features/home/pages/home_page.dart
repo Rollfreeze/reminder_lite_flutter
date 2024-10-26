48852c9d4ac9741013403bd386f184fa383c3836
Rollfire <goronos2000@gmail.com>
2024-10-26 11:28:48 +0300

    refactor: unused imports

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,11 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:reminder_lite/core/services/localization_service.dart';
 import 'package:reminder_lite/core/style/app_colors.dart';
-import 'package:reminder_lite/core/style/app_radius.dart';
-import 'package:reminder_lite/core/style/app_typo.dart';
 import 'package:reminder_lite/core/widgets/add_reminder_button.dart';
 import 'package:reminder_lite/features/home/widgets/categories_section/categories_section.dart';
 import 'package:reminder_lite/features/home/widgets/progress_section/progress_section.dart';
