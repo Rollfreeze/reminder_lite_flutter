a084578d9fd3a671d3f1e39ead6e4cb8d007db2d
Rollfire <goronos2000@gmail.com>
2024-10-28 22:59:16 +0300

    wip: text area

--- lib/core/style/app_shadows.dart
+++ lib/core/style/app_shadows.dart
@@ -9,4 +9,12 @@ class AppShadows {
       color: Color(0x00C9C9C9),
     ),
   ];
+
+  static const shadowSecondary = [
+    BoxShadow(
+      offset: Offset(0, 2),
+      blurRadius: 4,
+      color: Color.fromRGBO(201, 201, 201, 0.14),
+    ),
+  ];
 }
--- lib/features/new_reminder/new_reminder_screen.dart
+++ lib/features/new_reminder/new_reminder_screen.dart
@@ -1,7 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:reminder_lite/core/services/localization_service.dart';
 import 'package:reminder_lite/core/style/app_colors.dart';
+import 'package:reminder_lite/core/style/app_radius.dart';
+import 'package:reminder_lite/core/style/app_shadows.dart';
 import 'package:reminder_lite/features/new_reminder/new_reminder_app_bar.dart';
 
 class NewReminderScreen extends StatefulWidget {
@@ -28,10 +31,19 @@ class _NewReminderScreenState extends State<NewReminderScreen> {
             onCancel: context.router.maybePop,
             onAdd: () => throw UnimplementedError(),
           ),
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16, 43, 16, 20),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 43, 16, 20),
             child: CupertinoTextField(
               maxLines: 4,
+              placeholder: "${LocalizationService.locale.name}\n\n\n",
+              placeholderStyle: const TextStyle(
+                color: AppColors.gray1,
+              ),
+              decoration: const BoxDecoration(
+                borderRadius: AppRadius.radius12,
+                color: AppColors.white,
+                boxShadow: AppShadows.shadowSecondary,
+              ),
             ),
           ),
         ],
