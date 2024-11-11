cae9fad0bee986872b1e682de81a27e955a559bb
Rollfire <goronos2000@gmail.com>
2024-11-11 18:42:25 +0300

    wip: MultiDatePicker

--- ios/Runner/Common/MaybeDatePicker.swift
+++ ios/Runner/Common/MaybeDatePicker.swift
@@ -2,29 +2,35 @@ import SwiftUI
 
 struct MaybeDatePicker: View {
     @State private var isOn: Bool = false
+    @State private var selectedDates: Set<DateComponents> = []
     
     var body: some View {
-        HStack {
-            Toggle(isOn: $isOn) {
-                HStack {
-                    Image(systemName: "calendar")
-                        .foregroundColor(.white)
-                        .padding(.vertical, 5)
-                        .padding(.horizontal, 4.5)
-                        .background(Color.red)
-                        .clipShape(RoundedRectangle(cornerRadius: 4))
-                    
-                    Text("Date")
-                        .padding(.horizontal, 4)
+        VStack {
+            HStack {
+                Toggle(isOn: $isOn) {
+                    HStack {
+                        Image(systemName: "calendar")
+                            .foregroundColor(.white)
+                            .padding(.vertical, 5)
+                            .padding(.horizontal, 4.5)
+                            .background(Color.red)
+                            .clipShape(RoundedRectangle(cornerRadius: 4))
+                        
+                        Text("Date")
+                            .padding(.horizontal, 4)
+                    }
                 }
             }
+            .padding(.horizontal)
+            .padding(.vertical, 10)
+            .background(Color.white)
         }
-        .padding(.horizontal)
-        .padding(.vertical, 10)
-        .background(Color.white)
+        if isOn {
+            MultiDatePicker("Label", selection: $selectedDates)
+                .transition(.move(edge: .top))
+                .animation(.easeInOut, value: isOn)
+        }
         
-        //        DatePicker(selection: /*@START_MENU_TOKEN@*/.constant(Date())/*@END_MENU_TOKEN@*/, label: { /*@START_MENU_TOKEN@*/Text("Date")/*@END_MENU_TOKEN@*/ })
-        //        MultiDatePicker(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Binding<Set<DateComponents>>@*/.constant([])/*@END_MENU_TOKEN@*/)
     }
 }
 
