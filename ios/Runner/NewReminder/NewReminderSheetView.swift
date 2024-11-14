7e1f150788237704422dff3893de724136c2db04
Rollfire <goronos2000@gmail.com>
2024-11-14 20:26:34 +0300

    refactor: add navigationBarTitleDisplayMode

--- ios/Runner/NewReminder/NewReminderSheetView.swift
+++ ios/Runner/NewReminder/NewReminderSheetView.swift
@@ -19,6 +19,7 @@ struct NewReminderSheetView: View {
                 }
             }
             .toolbar { BottomSheetAppBar(onCancel: onCancel, onAdd: onCancel) }
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
