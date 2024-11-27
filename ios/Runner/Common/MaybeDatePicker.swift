afb041a55e80a8d9f7e43dcc7b6a63851345c83a
Rollfire <goronos2000@gmail.com>
2024-11-27 22:31:47 +0300

    wip: add formatDateIfSelected

--- ios/Runner/Common/MaybeDatePicker.swift
+++ ios/Runner/Common/MaybeDatePicker.swift
@@ -11,7 +11,7 @@ struct MaybeDatePicker: View {
                 imageName: "calendar",
                 bgColor: Color.red,
                 title: "Date",
-                subTitle: "selectedDateFormated",
+                subTitle: formatDateIfSelected(),
                 isToggleOn: $isDatePickerActive
             )
             
@@ -22,6 +22,30 @@ struct MaybeDatePicker: View {
                 MultiDatePicker("Date", selection: $selectedDates)
                     .padding(.horizontal)
             }
+        }
+    }
+    
+    private func formatDateIfSelected() -> String? {
+        guard let selectedDateComponents = selectedDates.first,
+              let selectedDate = Calendar.current.date(from: selectedDateComponents) else {
+            return nil
+        }
+        
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: Date())
+        let tomorrow = calendar.date(byAdding: .day, value: 1, to: today)!
+        let yesterday = calendar.date(byAdding: .day, value: -1, to: today)!
+        
+        if calendar.isDate(selectedDate, inSameDayAs: today) {
+            return "Today"
+        } else if calendar.isDate(selectedDate, inSameDayAs: tomorrow) {
+            return "Tomorrow"
+        } else if calendar.isDate(selectedDate, inSameDayAs: yesterday) {
+            return "Yesterday"
+        } else {
+            let formatter = DateFormatter()
+            formatter.dateStyle = .full // Gives the format like "Friday, 29 November 2024"
+            return formatter.string(from: selectedDate)
         }
     }
 }
