c35c9d7f63d61a7611788b91cc670964b4b519fb
Rollfire <goronos2000@gmail.com>
2024-12-31 20:18:47 +0300

    refactor: pressing the toggle improvement

--- ios/Runner/Common/ListTileSwitcher.swift
+++ ios/Runner/Common/ListTileSwitcher.swift
@@ -15,49 +15,53 @@ struct ListTileSwitcher: View {
     @State private var isPressed = false
     
     var body: some View {
-        HStack {
-            Toggle(isOn: Binding(
-                get: { isToggleOn },
-                set: onToggleChange
-            )) {
-                HStack {
-                    Image(systemName: imageName)
-                        .foregroundColor(.white)
-                        .padding(.vertical, 5)
-                        .padding(.horizontal, 4.5)
-                        .background(bgColor)
-                        .clipShape(RoundedRectangle(cornerRadius: 4))
-                    
-                    VStack(alignment: .leading) {
-                        Text(title)
-                            .offset(y: subTitle == nil ? 0 : 2)
-                        if let subTitle = subTitle {
-                            Text(subTitle)
-                                .font(.subheadline)
-                                .foregroundStyle(.blue)
-                                .offset(x: 1)
+        ZStack {
+            HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.white)
+                    .padding(.vertical, 5)
+                    .padding(.horizontal, 4.5)
+                    .background(bgColor)
+                    .clipShape(RoundedRectangle(cornerRadius: 4))
+                
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .offset(y: subTitle == nil ? 0 : 2)
+                    if let subTitle = subTitle {
+                        Text(subTitle)
+                            .font(.subheadline)
+                            .foregroundStyle(.blue)
+                            .offset(x: 1)
+                    }
+                }
+                .padding(.horizontal, 4)
+                Spacer()
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 10)
+            .background(isPressed ? Color.gray.opacity(0.4) : Color.white)
+            .animation(.easeInOut(duration: 0.25), value: isPressed)
+            .simultaneousGesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged { _ in
+                        if (isToggleOn && !isPressed) {
+                            isPressed = true
+                            onActivePressed()
                         }
                     }
-                    .padding(.horizontal, 4)
-                }
-            }
-        }
-        .padding(.horizontal)
-        .padding(.vertical, 10)
-        .background(isPressed ? Color.gray.opacity(0.4) : Color.white)
-        .animation(.easeInOut(duration: 0.25), value: isPressed)
-        .simultaneousGesture(
-            DragGesture(minimumDistance: 0)
-                .onChanged { _ in
-                    if (isToggleOn && !isPressed) {
-                        isPressed = true
-                        onActivePressed()
+                    .onEnded { _ in
+                        isPressed = false
                     }
-                }
-                .onEnded { _ in
-                    isPressed = false
-                }
-        )
+            )
+            
+            Toggle(
+                isOn: Binding(
+                    get: { isToggleOn },
+                    set: onToggleChange
+                )
+            ) {}
+            .padding(.horizontal)
+        }
         .frame(maxHeight: 50)
     }
 }
