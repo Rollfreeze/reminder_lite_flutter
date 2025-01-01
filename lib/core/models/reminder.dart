258c421fd5e91bd7a0af6e99bfb6be536ce1dd0c
Rollfire <goronos2000@gmail.com>
2025-01-01 17:39:31 +0300

    wip: add Reminder model

---
+++ lib/core/models/reminder.dart
@@ -0,0 +1,14 @@
+/// A model that represents and contains all about some reminder.
+class Reminder {
+  final String id;
+  final String title;
+  final String notes;
+  final DateTime date;
+
+  const Reminder({
+    required this.id,
+    required this.title,
+    required this.notes,
+    required this.date,
+  });
+}
