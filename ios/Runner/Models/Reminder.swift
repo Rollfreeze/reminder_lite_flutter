aec6e42f39459265a549d8af026d426cbc4650c2
Rollfire <goronos2000@gmail.com>
2025-01-03 15:03:34 +0300

    refactor: Reminder model naming

--- ios/Runner/Models/Reminder.swift
+++ ios/Runner/Models/Reminder.swift
@@ -1,22 +1,20 @@
 /// A model that represents and contains all about some certain reminder.
-struct ReminderModel {
+struct Reminder {
     let id: String
     let title: String
     let notes: String
-    let date: Date
+    let date: Date?
     
     /// A new reminder.
-    init(title: String, notes: String, date: Date, time: Date) {
-        let fullDate = date.addingTimeInterval(time.timeIntervalSinceReferenceDate)
-    
+    init(title: String, notes: String, date: Date?, time: Date?) {
         self.id = UUID().uuidString
         self.title = title
         self.notes = notes
-        self.date = fullDate
+        self.date = date?.addingTimeInterval(time?.timeIntervalSinceReferenceDate ?? 0)
     }
     
     /// An existing reminder.
-    init(id: String, title: String, notes: String, date: Date) {
+    init(id: String, title: String, notes: String, date: Date?) {
         self.id = id
         self.title = title
         self.notes = notes
