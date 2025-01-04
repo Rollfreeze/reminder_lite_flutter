f4b2030be1f633ceb739242ff8ebf34f81d5301b
Rollfire <goronos2000@gmail.com>
2025-01-04 23:48:56 +0300

    chore: add withTimeOnly extension

--- ios/Runner/Extensions/DateExtensions.swift
+++ ios/Runner/Extensions/DateExtensions.swift
@@ -5,4 +5,11 @@ extension Date {
         let dateComponents = calendar.dateComponents([.year, .month, .day], from: self)
         return calendar.date(from: dateComponents)
     }
+    
+    var withTimeOnly: Date? {
+        var calendar = Calendar.current
+        calendar.timeZone = .gmt
+        var dateComponents = calendar.dateComponents([.hour, .minute, .second], from: self)
+        return calendar.date(from: dateComponents)
+    }
 }
