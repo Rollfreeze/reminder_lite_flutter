66e630dc48f466576d3f2ce8ccccc81d53391a43
Rollfire <goronos2000@gmail.com>
2025-01-05 18:04:34 +0300

    refactor: replace withTimeOnly to replaceTimeFrom

--- ios/Runner/Extensions/DateExtensions.swift
+++ ios/Runner/Extensions/DateExtensions.swift
@@ -6,10 +6,28 @@ extension Date {
         return calendar.date(from: dateComponents)
     }
     
-    var withTimeOnly: Date? {
+    /// Change current date time to the passed one instead.
+    ///
+    /// Returns the same date if passed date is nil.
+    func replaceTimeFrom(_ date: Date?) -> Date {
+        guard date != nil else { return self }
+        
         var calendar = Calendar.current
         calendar.timeZone = .gmt
-        let dateComponents = calendar.dateComponents([.hour, .minute, .second], from: self)
-        return calendar.date(from: dateComponents)
+        
+        let dateComponents = calendar.dateComponents([.year, .month, .day], from: self)
+        let timeComponents = calendar.dateComponents([.hour, .minute, .second], from: date!)
+        
+        var combinedComponents = DateComponents()
+        
+        combinedComponents.year = dateComponents.year
+        combinedComponents.month = dateComponents.month
+        combinedComponents.day = dateComponents.day
+        
+        combinedComponents.hour = timeComponents.hour
+        combinedComponents.minute = timeComponents.minute
+        combinedComponents.second = timeComponents.second
+        
+        return calendar.date(from: combinedComponents)!
     }
 }
