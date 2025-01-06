576f65762a9fc8f3771b5af2e88f386928154e00
Rollfire <goronos2000@gmail.com>
2025-01-06 13:00:18 +0300

    fix: autoRouterConfig
    After all pages was renamed as Screens, @AutoRouterConfig() had to be changed as well, but I forgot to do it then so doing it now

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -8,7 +8,7 @@ import 'package:reminder_lite/features/today/today_screen.dart';
 
 part 'app_router.gr.dart';
 
-@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+@AutoRouterConfig()
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
