cb20ce38298a7728a51dcc66bea22b331d443d9f
Rollfire <goronos2000@gmail.com>
2025-01-07 23:33:45 +0300

    wip: Reminder.fromJson (Dart side)

--- lib/core/models/reminder.dart
+++ lib/core/models/reminder.dart
@@ -3,7 +3,7 @@ class Reminder {
   final String id;
   final String title;
   final String notes;
-  final DateTime date;
+  final DateTime? date;
 
   const Reminder({
     required this.id,
@@ -11,4 +11,17 @@ class Reminder {
     required this.notes,
     required this.date,
   });
+
+  // Factory constructor to create an instance from JSON
+  factory Reminder.fromJson(Map<String, dynamic> json) => Reminder(
+        id: json['id'] as String,
+        title: json['title'] as String,
+        notes: json['notes'] as String,
+        date: json['date'] == null
+            ? null
+            : DateTime.fromMillisecondsSinceEpoch(
+                isUtc: true,
+                ((json['date'] as double) * 1000).toInt(),
+              ),
+      );
 }
