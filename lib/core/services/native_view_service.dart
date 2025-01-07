8203c367a3790f4102dafb6443d63cf24f6cf32c
Rollfire <goronos2000@gmail.com>
2025-01-07 23:35:09 +0300

    feat: ReminderService create method

--- lib/core/services/native_view_service.dart
+++ lib/core/services/native_view_service.dart
@@ -1,12 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:reminder_lite/core/models/reminder.dart';
 
 /// Service for native views call.
 class ReminderService {
   static const _name = "ReminderServiceMethodChannel";
   static const _methodChannel = MethodChannel(_name);
 
-  static Future<void> create() async {
-    final result = await _methodChannel.invokeMethod("create");
-    print(result);
+  static Future<Reminder?> create() async {
+    try {
+      final result = await _methodChannel.invokeMethod("create");
+      if (result == null) return null;
+      final reminder = Reminder.fromJson(Map<String, dynamic>.from(result));
+      if (kDebugMode) print("Created Reminder: ${reminder.propertiesFormated}");
+      return reminder;
+    } catch (e) {
+      if (kDebugMode) print('Create Reminder parse Error: $e');
+      return null;
+    }
   }
 }
