b12ea8d47d1ca2e9268f5ed13aaf8e81f196a3ec
Rollfire <goronos2000@gmail.com>
2025-01-18 14:09:31 +0300

    chore: update bloc & flutter_bloc

--- lib/features/home/domain/bloc/progress_bloc/progress_bloc.dart
+++ lib/features/home/domain/bloc/progress_bloc/progress_bloc.dart
@@ -1,4 +1,4 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:reminder_lite/features/home/domain/models/progress_circle_model.dart';
 import 'package:reminder_lite/core/models/reminder_categories.dart';
