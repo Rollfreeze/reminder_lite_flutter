5a79d06b8e8bd31f074043619f7dd54e01c1b70d
Egor <71181173+Rollfreeze@users.noreply.github.com>
2025-01-24 22:27:44 +0300

    Add List of Repeat options

--- ios/Runner/Views/ReminderRepeatView.swift
+++ ios/Runner/Views/ReminderRepeatView.swift
@@ -2,8 +2,85 @@ import SwiftUI
 
 struct ReminderRepeatView: View {
     var body: some View {
-        HStack {
-            
+        NavigationStack {
+            ZStack {
+                Color.gray.opacity(0.15)
+                    .ignoresSafeArea()
+                
+                List {
+                    HStack {
+                        Text("Never")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Daily")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Weekdays")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Weekends")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Weekly")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Fortnightly")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Monthly")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Every 3 Months")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Every 6 Months")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+                    
+                    HStack {
+                        Text("Yearly")
+                        Spacer()
+                    }
+                    .padding(.vertical, 4)
+
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Repeat")
+                        .font(.headline)
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
+}
+
+#Preview {
+    ReminderRepeatView()
 }
