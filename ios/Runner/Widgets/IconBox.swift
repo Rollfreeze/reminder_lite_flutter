04cd79ee0ff39d7787719e28c08b195e5a8fd30b
Egor <71181173+Rollfreeze@users.noreply.github.com>
2025-01-24 21:58:08 +0300

    Add IconBox

---
+++ ios/Runner/Widgets/IconBox.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct IconBox: View {
+    let image: String
+    let boxColor: Color
+    
+    var body: some View {
+        ZStack {
+            boxColor
+                .frame(width: 30, height: 30)
+                .cornerRadius(8)
+            
+            Image(systemName: image)
+                .foregroundColor(.white)
+        }
+    }
+}
