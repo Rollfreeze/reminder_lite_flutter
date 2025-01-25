6368359504bb68ef893a7ca067983d14afd01b56
Egor <71181173+Rollfreeze@users.noreply.github.com>
2025-01-25 20:59:08 +0300

    Refactor ReminderTextInputSection
    I changed ReminderForm to ReminderTextInputSection. I also changed layout logic

---
+++ ios/Runner/Widgets/ReminderTextInputSection.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+/// Reminder's form section with title and notes fields.
+struct ReminderTextInputSection: View {
+    @EnvironmentObject var form: ReminderFormViewModel
+    
+    var body: some View {
+        Section {
+            TextField("Title", text: $form.title)
+            TextEditor(text: $form.notes)
+                .padding(.vertical, -8)
+                .padding(.horizontal, -4)
+                .frame(height: 100)
+                .overlay {
+                    if form.notes.isEmpty {
+                        VStack {
+                            HStack {
+                                Text("Notes")
+                                    .foregroundColor(.gray.opacity(0.5))
+                                Spacer()
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ReminderView(
+        onCancel: {},
+        onConfirm: { reminder in }
+    )
+}
