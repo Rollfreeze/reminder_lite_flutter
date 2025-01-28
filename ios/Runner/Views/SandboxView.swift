1ca2248b2e3732672c733e6e15a2eee68a2f8c1a
Egor <71181173+Rollfreeze@users.noreply.github.com>
2025-01-28 22:21:14 +0300

    Add all fonts for SandboxView to have example

--- ios/Runner/Views/SandboxView.swift
+++ ios/Runner/Views/SandboxView.swift
@@ -6,6 +6,36 @@ struct SandboxView: View {
     var body: some View {
         NavigationStack {
             VStack {
+                Text("Hello World!")
+                    .foregroundStyle(.secondary)
+                
+                Spacer()
+                
+                Text("Hello World!")
+                
+                Text("largeTitle!")
+                    .font(.largeTitle)
+                Text("title!")
+                    .font(.title)
+                Text("title2!")
+                    .font(.title2)
+                Text("title3!")
+                    .font(.title3)
+                Text("caption!")
+                    .font(.caption)
+                Text("caption2!")
+                    .font(.caption2)
+                Text("footnote!")
+                    .font(.footnote)
+                Text("headline!")
+                    .font(.headline)
+                Text("callout!")
+                    .font(.callout)
+                Text("subheadline!")
+                    .font(.subheadline)
+
+                Spacer()
+                
                 TextField("TextField", text: $text)
                     .border(Color.gray)
                 
