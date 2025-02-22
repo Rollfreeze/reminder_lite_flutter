fde7d730685bce78667706409c35e3238be8d6cf
Egor Ilin <71181173+Rollfreeze@users.noreply.github.com>
2025-02-22 23:20:53 +0300

    Add code for ReminderCategory

---
+++ ios/Runner/Models/ReminderCategory.swift
@@ -0,0 +1,18 @@
+enum ReminderCategory: Int, CaseIterable {
+    case today = 0
+    case month = 1
+    case all = 2
+    case done = 3
+    
+    /// Create `ReminderCategory` instance from a code.
+    static func from(code: Int) throws -> ReminderCategory {
+        guard let category = Self.allCases.first(where: { $0.rawValue == code }) else {
+            throw NSError(
+                domain: "RepeatOption",
+                code: 404,
+                userInfo: [NSLocalizedDescriptionKey: "No such code in options: \(code)"]
+            )
+        }
+        return category
+    }
+}
--- lib/core/models/reminder_categories.dart
+++ lib/core/models/reminder_categories.dart
@@ -4,26 +4,32 @@ import '../style/app_colors.dart';
 
 enum ReminderCategory {
   today(
+    code: 0,
     icon: CupertinoIcons.calendar_today,
     color: AppColors.blue,
   ),
   month(
+    code: 1,
     icon: CupertinoIcons.calendar,
     color: AppColors.red,
   ),
   all(
+    code: 2,
     icon: CupertinoIcons.cube_box,
     color: AppColors.black,
   ),
   done(
+    code: 3,
     icon: CupertinoIcons.checkmark,
     color: AppColors.gray5,
   );
 
+  final int code;
   final IconData icon;
   final Color color;
 
   const ReminderCategory({
+    required this.code,
     required this.icon,
     required this.color,
   });
