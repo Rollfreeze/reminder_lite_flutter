2c515b0e7a54badb0aa92cec0181b927cfc115f4
Egor Ilin <71181173+Rollfreeze@users.noreply.github.com>
2025-02-22 13:20:40 +0300

    Refactor toJson method for Swift Reminder model

--- ios/Runner/Models/Reminder.swift
+++ ios/Runner/Models/Reminder.swift
@@ -8,7 +8,6 @@ class Reminder: Identifiable {
     var title: String
     var notes: String
     var date: Date?
-//    var repeatance: RepeatanceOption
     var repeatanceCode: Int
     
     /// A new reminder.
@@ -17,18 +16,30 @@ class Reminder: Identifiable {
         self.title = title
         self.notes = notes
         self.date = date?.replaceTimeFrom(time)
-//        self.repeatance = repeatance
         self.repeatanceCode = repeatance.rawValue
     }
     
+    /// Get map representation of Reminder.
+    private func toMap() -> [String: Any] {
+        var map: [String: Any] = [
+            "repeatance_code": repeatanceCode
+        ]
+        
+        if !id.isEmpty { map["id"] = id }
+        if !title.isEmpty { map["title"] = title }
+        if !notes.isEmpty { map["notes"] = notes }
+        if let date = date { map["date"] = date.timeIntervalSince1970 }
+
+        return map
+    }
+    
     /// Get json representation of Reminder.
-    func toJson() -> [String: Any] {
-        var json: [String: Any] = [
-            "id": id,
-            "title": title,
-            "notes": notes,
-//            "repeatance": repeatance.rawValue,
-            "repeatance": repeatanceCode,
+    public func toJson() -> String? {
+        if let json = try? JSONSerialization.data(withJSONObject: toMap(), options: []) {
+            return String(data: json, encoding: .utf8)
+        }
+        return nil
+    }
         ]
         
         if let date = date {
