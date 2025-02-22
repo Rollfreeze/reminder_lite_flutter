0783d47c1800130fe7e97939548d10eac8d81d2b
Egor Ilin <71181173+Rollfreeze@users.noreply.github.com>
2025-02-22 20:08:23 +0300

    Replace ReminderCategory attributes

--- lib/core/models/reminder_categories.dart
+++ lib/core/models/reminder_categories.dart
@@ -3,32 +3,36 @@ import '../services/localization_service.dart';
 import '../style/app_colors.dart';
 
 enum ReminderCategory {
-  today(),
-  month(),
-  all(),
-  done();
+  today(
+    icon: CupertinoIcons.calendar_today,
+    color: AppColors.blue,
+  ),
+  month(
+    icon: CupertinoIcons.calendar,
+    color: AppColors.red,
+  ),
+  all(
+    icon: CupertinoIcons.cube_box,
+    color: AppColors.black,
+  ),
+  done(
+    icon: CupertinoIcons.checkmark,
+    color: AppColors.gray5,
+  );
 
-  /// A category localized name.
+  final IconData icon;
+  final Color color;
+
+  const ReminderCategory({
+    required this.icon,
+    required this.color,
+  });
+
+  /// Localized name based on the current locale.
   String get name => switch (this) {
         ReminderCategory.today => LocalizationService.locale.today,
         ReminderCategory.month => LocalizationService.locale.thisMonth,
         ReminderCategory.all => LocalizationService.locale.all,
         ReminderCategory.done => LocalizationService.locale.done,
-      };
-
-  /// A category icon.
-  IconData get icon => switch (this) {
-        ReminderCategory.today => CupertinoIcons.calendar_today,
-        ReminderCategory.month => CupertinoIcons.calendar,
-        ReminderCategory.all => CupertinoIcons.cube_box,
-        ReminderCategory.done => CupertinoIcons.checkmark,
-      };
-
-  /// A category color.
-  Color get color => switch (this) {
-        ReminderCategory.today => AppColors.blue,
-        ReminderCategory.month => AppColors.red,
-        ReminderCategory.all => AppColors.black,
-        ReminderCategory.done => AppColors.gray5,
       };
 }
