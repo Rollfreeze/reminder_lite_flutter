f49bc9cfa89ff31379216a3b11327896f4c796bc
Egor Ilin <71181173+Rollfreeze@users.noreply.github.com>
2025-02-25 23:00:23 +0300

    wip: add event "create" for ReminderEvent

--- lib/core/services/reminder_bloc/reminder_event.dart
+++ lib/core/services/reminder_bloc/reminder_event.dart
@@ -3,4 +3,5 @@ part of 'reminder_bloc.dart';
 @freezed
 sealed class ReminderEvent with _$ReminderEvent {
   const factory ReminderEvent.load() = _Load;
+  const factory ReminderEvent.create() = _Create;
 }
