b6de2675ebaf9604b23e89dbcf198a469a8aaf68
Egor Ilin <71181173+Rollfreeze@users.noreply.github.com>
2025-02-25 13:38:10 +0300

    wip: add ReminderBloc to ui

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import '../../../../core/services/reminder_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../core/services/reminder_bloc/reminder_bloc.dart';
 import '../../../../core/style/app_colors.dart';
-import '../../../../core/widgets/add_reminder_button.dart';
 import '../widgets/categories_section/categories_section.dart';
 import '../widgets/progress_section/progress_section.dart';
 
@@ -29,12 +30,17 @@ class HomeScreen extends StatelessWidget {
             children: [
               Expanded(
                 child: ListView(
-                  children: const [
-                    CupertinoSearchTextField(),
-                    SizedBox(height: 30),
-                    CategoriesSection(),
-                    SizedBox(height: 25),
-                    ProgressSection(),
+                  children: [
+                    const CupertinoSearchTextField(),
+                    const SizedBox(height: 30),
+                    BlocBuilder<ReminderBloc, ReminderState>(
+                      builder: (context, state) => state.map(
+                        loading: (_) => const CategoriesSection(),
+                        loaded: (state) => CategoriesSection(reminders: state.reminders),
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    const ProgressSection(),
                   ],
                 ),
               ),
--- lib/features/home/presentation/widgets/categories_section/categories_section.dart
+++ lib/features/home/presentation/widgets/categories_section/categories_section.dart
@@ -1,11 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
+import '../../../../../core/data/models/categorized_reminders.dart';
 import '../../../../../core/data/models/reminder_category.dart';
 import 'category_button.dart';
 
 /// A section with all reminder categories info.
 class CategoriesSection extends StatelessWidget {
-  const CategoriesSection({super.key});
+  final CategorizedReminders? reminders;
+
+  const CategoriesSection({this.reminders, super.key});
 
   static const _spacing = 16.0;
 
@@ -21,14 +24,14 @@ class CategoriesSection extends StatelessWidget {
             Flexible(
               child: CategoryButton(
                 category: ReminderCategory.today,
-                count: 10,
+                count: reminders?.today.length ?? 0,
                 onTap: () => context.router.pushNamed('today'),
               ),
             ),
             Flexible(
               child: CategoryButton(
                 category: ReminderCategory.month,
-                count: 20,
+                count: reminders?.month.length ?? 0,
                 onTap: () => context.router.pushNamed('month'),
               ),
             ),
@@ -40,14 +43,14 @@ class CategoriesSection extends StatelessWidget {
             Flexible(
               child: CategoryButton(
                 category: ReminderCategory.all,
-                count: 30,
+                count: reminders?.all.length ?? 0,
                 onTap: () => context.router.pushNamed('all'),
               ),
             ),
             Flexible(
               child: CategoryButton(
                 category: ReminderCategory.done,
-                count: 40,
+                count: reminders?.done.length ?? 0,
                 onTap: () => context.router.pushNamed('done'),
               ),
             ),
