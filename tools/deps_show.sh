3b9758a7397aacb90f19f3ee85373ad95ffc2aa6
Egor Ilin <71181173+Rollfreeze@users.noreply.github.com>
2025-03-03 00:21:06 +0300

    docs: update graph

--- tools/deps_show.sh
+++ tools/deps_show.sh
@@ -5,7 +5,9 @@ exclude2='**.g.dart'
 exclude3='**.gr.dart'
 exclude4='l10n/**'
 exclude5='core/**'
+exclude6='**/*_event.dart'
+exclude7='**/*_state.dart'
 
 cd ..
-lakos lib/ -i "{$exclude1,$exclude2,$exclude3,$exclude4,$exclude5}" --metrics > deps.dot
+lakos lib/ -i "{$exclude1,$exclude2,$exclude3,$exclude4,$exclude5,$exclude6,$exclude7}" --metrics > deps.dot
 dot -Tpng -Gdpi=200 -o deps.png deps.dot
