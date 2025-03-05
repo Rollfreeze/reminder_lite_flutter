7a99785b03dd0a4c99a3a27a44b437b29478a876
Egor Ilin <71181173+Rollfreeze@users.noreply.github.com>
2025-03-05 20:55:13 +0300

    refactor: CupertinoNavigationBar instead of Sliver

--- lib/features/listing/presentation/screens/listing_screen.dart
+++ lib/features/listing/presentation/screens/listing_screen.dart
@@ -8,42 +8,10 @@ import '../widgets/no_reminders_view.dart';
 import '../widgets/reminder_listing_item.dart';
 
 @RoutePage()
-class ListingScreen extends StatefulWidget {
+class ListingScreen extends StatelessWidget {
   final ReminderCategory category;
 
   const ListingScreen({required this.category, super.key});
-
-  @override
-  State<ListingScreen> createState() => _ListingScreenState();
-}
-
-class _ListingScreenState extends State<ListingScreen> {
-  final _controller = ScrollController();
-  bool _isTitleCollapsed = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller.addListener(_titleColorHandler);
-  }
-
-  @override
-  void dispose() {
-    _controller.removeListener(_titleColorHandler);
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _titleColorHandler() {
-    const collapseOffset = 40.0;
-
-    final isCollapsed = _controller.offset > collapseOffset;
-    final shouldCollapse = isCollapsed != _isTitleCollapsed;
-
-    if (shouldCollapse) setState(() => _isTitleCollapsed = isCollapsed);
-  }
-
-  Color get _titleColor => _isTitleCollapsed ? AppColors.black : widget.category.color;
 
   @override
   Widget build(BuildContext context) {
@@ -56,42 +24,35 @@ class _ListingScreenState extends State<ListingScreen> {
     );
 
     return CupertinoPageScaffold(
-      child: BlocBuilder<ReminderBloc, ReminderState>(
-        builder: (_, state) {
-          final group = state.reminders?.getBy(widget.category);
-          return CustomScrollView(
-            controller: _controller,
-            physics: const AlwaysScrollableScrollPhysics(),
-            slivers: <Widget>[
-              CupertinoSliverNavigationBar(
-                largeTitle: Text(
-                  widget.category.name,
-                  style: TextStyle(color: _titleColor),
+      navigationBar: CupertinoNavigationBar.large(
+        largeTitle: Text(
+          category.name,
+          style: TextStyle(color: category.color),
+        ),
+      ),
+      child: SafeArea(
+        child: BlocBuilder<ReminderBloc, ReminderState>(
+          builder: (context, state) {
+            final group = state.reminders?.getBy(category);
+
+            if (group == null || group.reminders.isEmpty) {
+              return const NoRemindersView();
+            }
+
+            return ListView.separated(
+              padding: const EdgeInsets.only(top: 40),
+              itemCount: state.reminders!.getBy(category).length,
+              separatorBuilder: (_, __) => separator,
+              itemBuilder: (_, index) => Padding(
+                padding: const EdgeInsets.fromLTRB(4, 8, 18, 8),
+                child: ReminderListingItem(
+                  reminder: group.reminders[index],
+                  onChanged: (value) {},
                 ),
               ),
-              const SliverToBoxAdapter(child: separator),
-              if (group == null || group.reminders.isEmpty)
-                const SliverFillRemaining(
-                  child: NoRemindersView(),
-                )
-              else
-                SliverPadding(
-                  padding: const EdgeInsets.only(bottom: 40),
-                  sliver: SliverList.separated(
-                    itemCount: group.length,
-                    separatorBuilder: (_, __) => separator,
-                    itemBuilder: (_, index) => Padding(
-                      padding: const EdgeInsets.fromLTRB(4, 8, 18, 8),
-                      child: ReminderListingItem(
-                        reminder: group.reminders[index],
-                        onChanged: (value) {},
-                      ),
-                    ),
-                  ),
-                ),
-            ],
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
