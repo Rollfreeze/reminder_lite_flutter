05804b605394bfbd2528be2fbc0d3e88aa136ba8
Egor Ilin <71181173+Rollfreeze@users.noreply.github.com>
2025-03-16 16:00:20 +0300

    refactor: RootViewService code style

--- ios/Runner/Services/RootViewService.swift
+++ ios/Runner/Services/RootViewService.swift
@@ -2,64 +2,77 @@ import SwiftUI
 
 /// Service class that can call iOS views for Flutter.
 class RootViewService {
-    /// Returns the most root `UIViewController` in iOS application.
-    static func getController() -> UIViewController? {
-        let rootController = UIApplication.shared.connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .flatMap { $0.windows }
-            .first { $0.isKeyWindow }?
-            .rootViewController
-        
+    /// Returns the most root `UIViewController` in the iOS application.
+    static func getRootController() -> UIViewController? {
+        guard let rootController = UIApplication.shared.connectedScenes
+            .compactMap({ $0 as? UIWindowScene })
+            .flatMap({ $0.windows })
+            .first(where: { $0.isKeyWindow })?
+            .rootViewController else {
+            return nil
+        }
         return rootController
     }
     
-    /// Calls a bottom sheet passing the `builder` as a body to it.
+    /// Presents a bottom sheet with the provided SwiftUI `View`.
     static func presentFullBottomSheet<Content: View>(
         onUserDismissView: @escaping () -> Void,
         @ViewBuilder builder: @escaping (_ onClose: @escaping () -> Void) -> Content
-    ) -> Void {
-        let viewController = ContentViewController()
-        viewController.onDidDisappear = {
-            onUserDismissView()
-        }
+    ) {
+        guard let rootController = getRootController() else { return }
         
-        let viewHostingController = UIHostingController(rootView: builder({
-            viewController.dismiss(animated: true, completion: nil)
-        }))
+        let contentViewController = ContentViewController()
+        contentViewController.setOnDisappearHandler(onUserDismissView)
         
-        viewController.addChild(viewHostingController)
-        viewController.view.addSubview(viewHostingController.view)
+        let hostingController = UIHostingController(rootView: builder {
+            contentViewController.dismiss(animated: true, completion: nil)
+        })
         
-        viewHostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        embedHostingController(hostingController, into: contentViewController)
+        
+        let navController = createNavigationController(root: contentViewController)
+        rootController.present(navController, animated: true, completion: nil)
+    }
+    
+    /// Embeds a `UIHostingController` into a given parent `UIViewController`.
+    private static func embedHostingController(_ hostingController: UIHostingController<some View>, into parent: UIViewController) {
+        parent.addChild(hostingController)
+        parent.view.addSubview(hostingController.view)
+        
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            viewHostingController.view.topAnchor.constraint(equalTo: viewController.view.topAnchor),
-            viewHostingController.view.leadingAnchor.constraint(equalTo: viewController.view.leadingAnchor),
-            viewHostingController.view.trailingAnchor.constraint(equalTo: viewController.view.trailingAnchor),
-            viewHostingController.view.bottomAnchor.constraint(equalTo: viewController.view.bottomAnchor)
+            hostingController.view.topAnchor.constraint(equalTo: parent.view.topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: parent.view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: parent.view.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: parent.view.bottomAnchor)
         ])
-        viewHostingController.didMove(toParent: viewController)
         
-        let nav = UINavigationController(rootViewController: viewController)
-        nav.modalPresentationStyle = .pageSheet
-        nav.navigationBar.isHidden = true
+        hostingController.didMove(toParent: parent)
+    }
+    
+    /// Creates a `UINavigationController` with a bottom sheet presentation style.
+    private static func createNavigationController(root: UIViewController) -> UINavigationController {
+        let navController = UINavigationController(rootViewController: root)
+        navController.modalPresentationStyle = .pageSheet
+        navController.navigationBar.isHidden = true
         
-        if let sheet = nav.sheetPresentationController {
+        if let sheet = navController.sheetPresentationController {
             sheet.detents = [.large()]
         }
         
-        
-        self.getController()?.present(nav, animated: true, completion: nil)
+        return navController
     }
 }
 
-/// Custom `UIViewController` for `View` that is provided to the `RootViewService`.
-///
-/// The main goal of this ViewController is ability to influence the life cycle of the View.
+/// Custom `UIViewController` to manage the lifecycle of the presented SwiftUI `View`.
 fileprivate class ContentViewController: UIViewController {
-    var onDidDisappear: (() -> Void)? = nil
+    private var onDidDisappear: (() -> Void)?
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
+    func setOnDisappearHandler(_ handler: @escaping () -> Void) {
+        onDidDisappear = handler
+    }
+    
+    deinit {
         onDidDisappear?()
     }
 }
